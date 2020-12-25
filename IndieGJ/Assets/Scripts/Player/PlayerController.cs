08443c97c7de5ff845d5cbafc548142ad4c5147b
AoqiWang <1017802402@qq.com>
2020-12-25 18:31:31 +0800

    Portal
    传送门

---
+++ IndieGJ/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveHorizontal, moveVertical;
+    [SerializeField]private float moveSpeed = 50f;
+    Vector3 deltaMovement = Vector3.zero;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+    //private void FixedUpdate() 
+    //{
+    //    rb.velocity = new Vector2(moveHorizontal*moveSpeed, moveVertical*moveSpeed);
+    //}
+    void Move()
+    {
+        //moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //moveVertical = Input.GetAxisRaw("Vertical");  
+        this.transform.position += deltaMovement;
+    }
+}
---
+++ IndieGJ/Assets/Scripts/bricks/Portal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 pos;//摄像机移动的位置
+    [SerializeField]
+    private Camera mainCamera;//摄像机
+    [SerializeField]
+    private GameObject player,associatedPortal;//玩家和相关联的传送门
+    [SerializeField]
+    private bool canGo = false;//是否可以传送
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+            canGo = true;   
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canGo = false;
+    }
+    private void MovePlayer()
+    {
+        player.transform.position = associatedPortal.transform.position;
+    }
+    private void MoveCamera()
+    {
+        mainCamera.transform.position = pos;
+    }
+
+    public void PortalTo()
+    {
+        if(canGo)
+        {
+            MoveCamera();
+            MovePlayer();
+        }
+    }
+}
